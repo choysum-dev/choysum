4f567fddd5986d93ffa92aed7f0819c7d0d3eea6
Brian Wang <wangbuke@gmail.com>
2026-06-24 12:58:55 +0800

    refactor(grpcclient): use repo-local minimal auth proto contract (#104)

--- pkg/auth/grpcclient/generate.go
+++ pkg/auth/grpcclient/generate.go
@@ -3,4 +3,4 @@
 
 package grpcclient
 
-//go:generate protoc -I ../../../.choysum/generated/proto/auth -I ../../../internal/module/artifact/generate/assets --go_out=paths=source_relative:./authpb --go-grpc_out=paths=source_relative:./authpb ../../../.choysum/generated/proto/auth/auth.proto
+//go:generate protoc -I . --go_out=paths=source_relative:./authpb --go-grpc_out=paths=source_relative:./authpb ./auth.proto
