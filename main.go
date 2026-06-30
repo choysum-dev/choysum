7e7a3392b8f4b30110de9d318dad07dad8396b31
buke <wangbuke@gmail.com>
2026-06-30 22:23:48 +0800

    fix(main): restore CLI entrypoint after cmd refactor
    - Restore root main package entrypoint in main.go with build-version resolution and Commander wiring.
    
    - Re-enable executable root build target so go build . succeeds again after cmd refactor commits removed main.go.
    
    - Preserve existing main package test seams (newCommander, exitFunc, readBuildInfo) used by main_test.go.

---
+++ main.go
@@ -0,0 +1,78 @@
+// SPDX-FileCopyrightText: 2026-present Brian Wang <wangbuke@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"runtime/debug"
+
+	"github.com/choysum-dev/choysum/cmd"
+	_ "github.com/choysum-dev/choysum/internal/initialize"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+
+	readBuildInfo = debug.ReadBuildInfo
+)
+
+func shortCommitHash(value string) string {
+	if len(value) > 7 {
+		return value[:7]
+	}
+	return value
+}
+
+func getBuildVersion() string {
+	v := version
+	c := commit
+	d := date
+	if version == "dev" {
+		if info, ok := readBuildInfo(); ok && info != nil {
+			revision := ""
+			dirty := false
+			for _, setting := range info.Settings {
+				if setting.Key == "vcs.revision" {
+					revision = shortCommitHash(setting.Value)
+				}
+				if setting.Key == "vcs.time" {
+					d = setting.Value
+				}
+				if setting.Key == "vcs.modified" && setting.Value == "true" {
+					dirty = true
+				}
+			}
+			if info.Main.Version != "" && info.Main.Version != "(devel)" {
+				v = info.Main.Version
+				if revision != "" {
+					c = revision
+				}
+			} else if revision != "" {
+				c = revision
+				v = fmt.Sprintf("dev-%s", revision)
+				if dirty {
+					v += "-dirty"
+				}
+			}
+		}
+	}
+	return fmt.Sprintf("%s (commit: %s, date: %s)", v, c, d)
+}
+
+var newCommander = func(ctx context.Context) interface{ Execute() error } {
+	return cmd.NewCommander(ctx, getBuildVersion())
+}
+
+var exitFunc = os.Exit
+
+func main() {
+	command := newCommander(context.Background())
+	if err := command.Execute(); err != nil {
+		exitFunc(1)
+	}
+}
